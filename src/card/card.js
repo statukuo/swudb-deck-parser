2f07a07df0efd3bc4a096b79a950d3ee3a425115
David Barbero <dbdstk@gmail.com>
2025-08-02 22:07:03 +0200

    Add check to card

--- src/card/card.js
+++ src/card/card.js
@@ -7,16 +7,17 @@ import { useSelector } from 'react-redux';
 export default function Card({ openPreview, defaultImagePath, count, defaultRarity, cardName, aspects, defaultCardNumber, nth, set, trilogy, limit }) {
     const target = useRef(null);
     const [show, setShow] = useState(false);
+    const [alreadyAdded, setAlreadyAdded] = useState(false);
 
     const collectionCount = useSelector((state) => state.collection.cards.filter(({ Set, CardNumber }) => set === Set && parseInt(defaultCardNumber) === parseInt(CardNumber))[0]?.Count || 0);
 
     const carStyle = {
         color: "black",
-        backgroundColor: (nth % 2) === 0 ? "#C7C9CE" : "#E9EAEC"
+        backgroundColor: alreadyAdded? ((nth % 2) === 0 ? "#a8cf97" : "#deedd8"): ((nth % 2) === 0 ? "#C7C9CE" : "#E9EAEC")
     };
 
     return (
-        <Row onClick={() => openPreview(defaultImagePath, count, trilogy? limit : collectionCount)} style={carStyle} className="pt-2 pb-2">
+        <Row style={carStyle} className="pt-2 pb-2">
             <Overlay target={target.current} show={show} placement="right">
                 {({
                     placement: _placement,
@@ -41,12 +42,15 @@ export default function Card({ openPreview, defaultImagePath, count, defaultRari
                     </div>
                 )}
             </Overlay>
+            <Col className="p-0" xs="1">
+                <input class="form-check-input" type="checkbox" value={alreadyAdded} id="flexCheckDefault" onChange={() => setAlreadyAdded(!alreadyAdded)}></input>
+            </Col>
 
-            <Col className="p-0" xs="1" style={{ color: collectionCount >= count || trilogy ? "black" : "red" }} ref={target} onMouseEnter={() => collectionCount < count && setShow(true)} onMouseLeave={() => collectionCount < count && setShow(false)}>{count}x</Col>
-            <Col className="p-0" xs="1"><Rarity rarityNumber={defaultRarity} /></Col>
-            <Col className="p-0" xs="2">#{defaultCardNumber}</Col>
-            <Col className="p-0 text-start" xs="6">{cardName}</Col>
-            <Col className="p-0" xs="2">{aspects.map((aspect, idx) => <Aspect key={idx} aspectNumber={aspect} />)}</Col>
+            <Col className="p-0" onClick={() => openPreview(defaultImagePath, count, trilogy ? limit : collectionCount)} xs="1" style={{ color: collectionCount >= count || trilogy ? "black" : "red" }} ref={target} onMouseEnter={() => collectionCount < count && setShow(true)} onMouseLeave={() => collectionCount < count && setShow(false)}>{count}x</Col>
+            <Col className="p-0" onClick={() => openPreview(defaultImagePath, count, trilogy ? limit : collectionCount)} xs="1"><Rarity rarityNumber={defaultRarity} /></Col>
+            <Col className="p-0" onClick={() => openPreview(defaultImagePath, count, trilogy ? limit : collectionCount)} xs="2">#{defaultCardNumber}</Col>
+            <Col className="p-0 text-start" onClick={() => openPreview(defaultImagePath, count, trilogy ? limit : collectionCount)} xs="6">{cardName}</Col>
+            <Col className="p-0" onClick={() => openPreview(defaultImagePath, count, trilogy ? limit : collectionCount)} xs="1">{aspects.map((aspect, idx) => <Aspect key={idx} aspectNumber={aspect} />)}</Col>
         </Row>
     );
 }
